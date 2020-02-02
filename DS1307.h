7b71fccfd51aa5b4f29dc0dfd963ab0d3e693f8e
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 11:38:25 +0800

    Pretty printed the Arduino code with astyle

--- DS1307.h
+++ DS1307.h
@@ -1,32 +1,32 @@
 /*
- * DS1307.h
- * library for Seeed RTC module
- *
- * Copyright (c) 2013 seeed technology inc.
- * Author        :   FrankieChu 
- * Create Time   :   Jan 2013
- * Change Log    :
- *
- * The MIT License (MIT)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+    DS1307.h
+    library for Seeed RTC module
+
+    Copyright (c) 2013 seeed technology inc.
+    Author        :   FrankieChu
+    Create Time   :   Jan 2013
+    Change Log    :
+
+    The MIT License (MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
 
 #ifndef __DS1307_H__
 #define __DS1307_H__
@@ -43,13 +43,12 @@
 #define SAT 6
 #define SUN 7
 
-class DS1307
-{
-private:
+class DS1307 {
+  private:
     uint8_t decToBcd(uint8_t val);
     uint8_t bcdToDec(uint8_t val);
 
-public:
+  public:
     void begin();
     void startClock(void);
     void stopClock(void);
@@ -60,7 +59,7 @@ public:
     void fillDayOfWeek(uint8_t _dow);
     uint8_t second;
     uint8_t minute;
-    uint8_t hour; 
+    uint8_t hour;
     uint8_t dayOfWeek;// day of week, 1 = Monday
     uint8_t dayOfMonth;
     uint8_t month;
--- examples/SetTimeAndDisplay/SetTimeAndDisplay.ino
+++ examples/SetTimeAndDisplay/SetTimeAndDisplay.ino
@@ -1,9 +1,9 @@
-/****************************************************************************/	
-//	Function: Set time and get the time from RTC chip(DS1307) and display 
+/****************************************************************************/
+//	Function: Set time and get the time from RTC chip(DS1307) and display
 //			  it on the serial monitor.
 //	Hardware: Grove - RTC
 //	Arduino IDE: Arduino-1.0
-//	Author:	 FrankieChu		
+//	Author:	 FrankieChu
 //	Date: 	 Jan 19,2013
 //	Version: v1.0
 //	by www.seeedstudio.com
@@ -27,60 +27,56 @@
 #include "DS1307.h"
 
 DS1307 clock;//define a object of DS1307 class
-void setup()
-{
-	Serial.begin(9600);
-	clock.begin();
-	clock.fillByYMD(2013,1,19);//Jan 19,2013
-	clock.fillByHMS(15,28,30);//15:28 30"
-	clock.fillDayOfWeek(SAT);//Saturday
-	clock.setTime();//write time to the RTC chip
+void setup() {
+    Serial.begin(9600);
+    clock.begin();
+    clock.fillByYMD(2013, 1, 19); //Jan 19,2013
+    clock.fillByHMS(15, 28, 30); //15:28 30"
+    clock.fillDayOfWeek(SAT);//Saturday
+    clock.setTime();//write time to the RTC chip
 }
-void loop()
-{
-	printTime();
+void loop() {
+    printTime();
 }
 /*Function: Display time on the serial monitor*/
-void printTime()
-{
-	clock.getTime();
-	Serial.print(clock.hour, DEC);
-	Serial.print(":");
-	Serial.print(clock.minute, DEC);
-	Serial.print(":");
-	Serial.print(clock.second, DEC);
-	Serial.print("	");
-	Serial.print(clock.month, DEC);
-	Serial.print("/");
-	Serial.print(clock.dayOfMonth, DEC);
-	Serial.print("/");
-	Serial.print(clock.year+2000, DEC);
-	Serial.print(" ");
-	Serial.print(clock.dayOfMonth);
-	Serial.print("*");
-	switch (clock.dayOfWeek)// Friendly printout the weekday
-	{
-		case MON:
-		  Serial.print("MON");
-		  break;
-		case TUE:
-		  Serial.print("TUE");
-		  break;
-		case WED:
-		  Serial.print("WED");
-		  break;
-		case THU:
-		  Serial.print("THU");
-		  break;
-		case FRI:
-		  Serial.print("FRI");
-		  break;
-		case SAT:
-		  Serial.print("SAT");
-		  break;
-		case SUN:
-		  Serial.print("SUN");
-		  break;
-	}
-	Serial.println(" ");
+void printTime() {
+    clock.getTime();
+    Serial.print(clock.hour, DEC);
+    Serial.print(":");
+    Serial.print(clock.minute, DEC);
+    Serial.print(":");
+    Serial.print(clock.second, DEC);
+    Serial.print("	");
+    Serial.print(clock.month, DEC);
+    Serial.print("/");
+    Serial.print(clock.dayOfMonth, DEC);
+    Serial.print("/");
+    Serial.print(clock.year + 2000, DEC);
+    Serial.print(" ");
+    Serial.print(clock.dayOfMonth);
+    Serial.print("*");
+    switch (clock.dayOfWeek) { // Friendly printout the weekday
+        case MON:
+            Serial.print("MON");
+            break;
+        case TUE:
+            Serial.print("TUE");
+            break;
+        case WED:
+            Serial.print("WED");
+            break;
+        case THU:
+            Serial.print("THU");
+            break;
+        case FRI:
+            Serial.print("FRI");
+            break;
+        case SAT:
+            Serial.print("SAT");
+            break;
+        case SUN:
+            Serial.print("SUN");
+            break;
+    }
+    Serial.println(" ");
 }
